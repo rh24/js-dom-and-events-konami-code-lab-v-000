8e0bed21a1d3d5397a92c4b2e11b577fefcdab2e
Rebecca Hong <ms.rebeccahong@gmail.com>
2018-04-23 19:06:50 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,12 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
+function init(e) {
   // Write your JavaScript code inside the init() function
+  document.body.addEventListener('keydown', (e) => {
+    const key = parseInt(e.detail || e.which);
 
+    if (code.includes(key)) {
+      alert('You did it!')
+    }
+  })
 }
